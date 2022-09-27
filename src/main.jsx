24148b00ac7d37208a848fc7cf8ded99c93d5f7b
b0nz <elliantosarono.p@gmail.com>
2022-09-27 14:29:11 +0700

    upgrade react-router-dom v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Box, ChakraProvider, Heading } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
 import Room from "./pages/Room";
+import ErrorBoundary from "./ErrorBoundary";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "/room",
+    element: (
+      <Box
+        w="full"
+        minH="100vh"
+        bgColor="gray.900"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        color="white"
+      >
+        <Heading>Please enter your room id</Heading>
+      </Box>
+    ),
+    errorElement: <ErrorBoundary />,
+  },
+  {
+    path: "/room/:id",
+    element: <Room />,
+    errorElement: <ErrorBoundary />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/room/:id">
-            <Room />
-          </Route>
-        </Switch>
-      </Router>
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
